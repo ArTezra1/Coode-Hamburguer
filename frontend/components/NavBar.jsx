470f437dc7a3e34a09cecfa35fde1fcb776a0411
Arthur <arthurg.gmarques@gmail.com>
2025-02-07 21:54:58 -0300

    Atualizado a navBar.

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import clsx from 'clsx'
+import React, { useState } from 'react'
 
 const NavBar = () => {
+
+  const [selected, setSelected] = useState("")
+
+  function handleHover(item){
+    setSelected = item
+  }
+
   return (
-    <nav className='lg:hidden mt-4'>
-        ofeef
+    <nav className='lg:hidden mt-4 bg-zinc-900 py-2 px-4 rounded-3xl flex gap-4 sticky'>
+        <a href="" 
+        id='lanches' 
+        className={
+          clsx(selected == "lanche" ? "text-blue-500" : "text-red-500")
+          }
+          onClick={()=>handleHover("lanche")}
+          >
+          Lanches
+        </a>
+        <a href="" 
+        id='combos'
+        className={
+          clsx(selected == "combos" ? "text-blue-500" : "text-red-500")
+          }
+          onClick={()=>handleHover("combos")}
+        >
+          Combos
+        </a>
+        <a href="" id='bebidas'>
+          Bebidas
+        </a>
+        <a href="" id='alcool'>
+          Bebidas Alcoolicas
+        </a>
     </nav>
   )
 }
