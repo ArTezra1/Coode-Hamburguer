f6ed3d3d50a7a8812a0f0bf3630f344b38414ba7
Arthur <arthurg.gmarques@gmail.com>
2025-02-09 18:22:10 -0300

    Adicionado a fonte Poppins e animação de scroll.

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,10 +1,13 @@
 import Main from "../components/Main";
-import Header from "../components/Header";
 import Aside from "../components/Aside";
+
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] })
 
 export default function Home() {
   return (
-    <section className="flex w-screen h-screen bg-zinc-950 text-zinc-100 gap-2">
+    <section className={`flex w-screen h-screen bg-zinc-950 text-zinc-100 gap-2 ${poppins.className}`}>
       <div className="xs:hidden lg:block">
         <Aside></Aside>
       </div>
