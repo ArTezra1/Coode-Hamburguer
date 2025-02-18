da10d9b1bdd194dd35136f40a99f6734ad3279ce
Arthur <arthurg.gmarques@gmail.com>
2025-02-18 09:25:35 -0300

    Adicionado mais métodos de rotas.

--- backend/src/controllers/ClientesController.js
+++ backend/src/controllers/ClientesController.js
@@ -4,28 +4,29 @@ import Services from "../../services/ServicesController.js";
 
 class ClientesController extends Services {
     constructor() {
-        super(ClientesModel);
+        super(ClientesModel)
     }
 
-    // Sobrescrevendo o método de criação para tratar senha
     async criar(req, res, next) {
         try {
-            const { senha, ...dadosCliente } = req.body;
+            const { senha, ...dadosCliente } = req.body
 
             if (!senha) {
-                return res.status(400).json({ message: "Senha é obrigatória" });
+                return res.status(400).json({ message: "Senha é obrigatória." })
             }
 
-            const salt = await bcrypt.genSalt(10);
-            const senhaHash = await bcrypt.hash(senha, salt);
+            const salt = await bcrypt.genSalt(10)
+            const senhaHash = await bcrypt.hash(senha, salt)
 
-            const cliente = await this.model.create({ ...dadosCliente, senha: senhaHash });
-            return res.status(201).json(cliente);
+            const cliente = await this.model.create({ ...dadosCliente, senha: senhaHash })
+
+            return res.status(201).json(cliente)
+
         } catch (error) {
-            console.error("Erro ao criar cliente:", error);
-            next(error);
+            console.error("Erro ao criar cliente:", error)
+            next(error)
         }
     }
 }
 
-export default new ClientesController(); // Exporta como instância
+export default new ClientesController()
--- backend/src/routes/ClientesRoutes.js
+++ backend/src/routes/ClientesRoutes.js
@@ -4,13 +4,13 @@ import paginar from "../middlewares/Paginar.js"
 
 const routes = express.Router()
 
-routes.get("/clientes", (req, res, next) => ClientesController.listar(req, res, next));
+routes.get("/clientes", (req, res, next) => ClientesController.listar(req, res, next))
 
 // routes.get("/clientes/query", ClientesController.listarClientesPorFiltro, paginar)
 
-// routes.get("/clientes/:id", ClientesController.listarClientePorId)
+routes.get("/clientes/:id", (req, res, next) => ClientesController.buscarPorId(req, res, next))
 
-// routes.post("/clientes", ClientesController.cadastrarCliente)
+routes.post("/clientes", (req, res, next) => ClientesController.criar(req, res, next))
 
 // routes.put("/clientes/:id", ClientesController.atualizarCliente)
 
