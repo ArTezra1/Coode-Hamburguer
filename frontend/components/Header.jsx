9335074b6e07b01188f2f57c105e9791ee66f99f
Arthur <arthurg.gmarques@gmail.com>
2025-02-24 22:33:13 -0300

    Atualizado o header do site.

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,9 +1,20 @@
+import { Menu } from 'lucide-react'
 import React from 'react'
 
 const Header = () => {
+  const menuOpen = true
   return (
-    <header className='bg-zinc-900 w-full h-[34vh] flex flex-col items-center rounded-b-lg pt-4'>
-      <div className='flex flex-col items-center gap-4 w-[75%]'>
+    <header className='bg-zinc-900 w-full h-[34vh] flex flex-col items-center rounded-b-lg md:pt-4'>
+      <div className='md:hidden xs:block p-2 bg-zinc-800 w-full'>
+        <Menu className='cursor-pointer w-10 h-10'></Menu>
+        {
+          menuOpen ??
+          <div>
+            menu aberto
+          </div>
+        }
+      </div>
+      <div className='flex flex-col items-center gap-4 w-[75%] xs:mt-10'>
         <h1 className='font-semibold text-xl'>
           Bem-vindo a nossa Hamburgueria!
         </h1>
