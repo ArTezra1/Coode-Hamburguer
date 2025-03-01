a0fd501c424ba907d3175f1f9afd07b004aca882
Arthur <arthurg.gmarques@gmail.com>
2025-03-01 19:50:25 -0300

    Atualizado o estilo do carrinho e outros arquivos.

--- frontend/components/Carrinho.jsx
+++ frontend/components/Carrinho.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import clsx from 'clsx'
+import { X } from 'lucide-react'
+import React, { useState } from 'react'
 
 const Carrinho = ({ onClose }) => {
+    const [isConfirmed, setIsConfirmed] = useState(0)
+
+    function handleConfirmed(){
+        setIsConfirmed(+1)
+    }
+
   return (
-    <section className='absolute z-10 w-screen h-screen bg-zinc-950/60 flex items-center justify-center'>
-        <div className='bg-zinc-500 p-4 xs:w-[80vw] lg:w-[60vw] lg:h-[70vh] xs:h-[60vh] rounded-md flex flex-col gap-8'>
-            <div>
-                <div className='p-10 bg-red-400' onClick={onClose}></div>
-            </div>
-            <div className='flex gap-4'>
-                <div>
+    <section className='absolute z-10 w-screen h-screen bg-zinc-950/60 flex items-center justify-center text-zinc-100'>
+        <div className='bg-zinc-800 p-4 xs:w-[80vw] lg:w-[60vw] lg:h-[70vh] xs:h-[60vh] rounded-md flex gap-8'>
+            <div className='md:flex flex-1 mt-5 xs:hidden'>
+                <div className='flex flex-col items-center mt-6'>
+                    <div className={clsx(isConfirmed > 0 ? 'p-2 w-8 h-8 bg-green-500 rounded-full flex items-center justify-center text-black' : 'p-2 w-8 h-8 bg-white rounded-full flex items-center justify-center text-black')}>
+                        1
+                    </div>
+                    <p>Confirmar</p>
                 </div>
-                <div>
+                <div className='w-full relative'>
+                    <hr className='absolute w-[100%] mt-10 left-0'/>
+                </div>
+                <div className='flex flex-col items-center mt-6'>
+                    <div className={clsx(isConfirmed == 2 ? 'p-2 w-8 h-8 bg-green-500 rounded-full flex items-center justify-center text-black' : 'p-2 w-8 h-8 bg-white rounded-full flex items-center justify-center text-black')}>
+                        2
+                    </div>
+                    <p>Pagamento</p>
+                </div>
+            </div>
+            
+            <div className='flex-1 h-full w-full flex flex-col gap-2'>
+                <p className='relative flex justify-end items-start text-zinc-300'>
+                    <X className='w-10 h-10 cursor-pointer hover:text-zinc-100 transition-colors' onClick={onClose}></X>
+                </p>
+                <div className='flex flex-col gap-4 flex-1 border border-zinc-300 h-[80%] rounded-md'>
+                    <div>
+                        <h1>JOBKBL</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit accusamus dolores tenetur quod ipsa error perferendis suscipit velit dolorum cum deserunt commodi praesentium, laudantium doloremque neque voluptas libero quas ullam!</p>
+
+                        
+                    </div>
                 </div>
             </div>
         </div>
--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -2,25 +2,33 @@
 
 import { CircleUserRound, ShoppingCart } from 'lucide-react'
 import { useState } from 'react'
+import { createPortal } from 'react-dom'
+import Carrinho from './Carrinho'
 
 const Header = () => {
   const [items, setItems] = useState(0)
+
+  const [isOpenCart, setOpenCart] = useState(false)
 
   function renderItems() {
     setItems(+1)
   }
 
+  function openCart() {
+    setOpenCart(!isOpenCart)
+  }
+
   return (
-    <header className='bg-zinc-900 w-full h-[34vh] flex flex-col items-center rounded-b-lg md:pt-4'>
-      <div className='md:hidden xs:block xs:py-3 xs:px-6 bg-zinc-800 w-full'>
+    <header className='bg-zinc-900 w-full h-[34vh] flex flex-col items-center rounded-b-lg lg:pt-4'>
+      <div className='lg:hidden xs:block xs:py-3 xs:px-6 bg-zinc-800 w-full'>
         <div className='flex items-center justify-between'>
           <div className='relative cursor-pointer'>
-            <ShoppingCart className='w-10 h-10'></ShoppingCart>
+            <ShoppingCart className='w-10 h-10 text-zinc-300 hover:text-zinc-100 transition-colors' onClick={openCart}></ShoppingCart>
             {
               items > 0 && (
                 <div className='absolute w-5 h-5 bg-red-700 left-6 top-6 rounded-full text-center'>
-                  <p className='font-bold text-center'>
-
+                  <p className='flex items-center justify-center text-center'>
+                    {items}
                   </p>
                 </div>
               )
@@ -28,7 +36,7 @@ const Header = () => {
 
           </div>
           <div className='cursor-pointer'>
-            <CircleUserRound className='w-10 h-10'></CircleUserRound>
+            <CircleUserRound className='w-10 h-10 text-zinc-300 hover:text-zinc-100 transition-colors'></CircleUserRound>
           </div>
         </div>
       </div>
@@ -40,6 +48,13 @@ const Header = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, soluta voluptatum blanditiis, illum iure ullam accusamus dolor odit laboriosam porro iusto, earum aut doloribus incidunt explicabo possimus quae aliquam exercitationem.
         </p>
       </div>
+      {isOpenCart &&
+        createPortal(
+          <div className="fixed top-0 left-0 z-50">
+            <Carrinho onClose={() => setOpenCart(false)} />
+          </div>,
+          document.body
+        )}
     </header>
   )
 }
