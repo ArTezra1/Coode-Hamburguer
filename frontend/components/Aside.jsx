7a0ca2d286697cc0b0eaab99e27a312df6e5a735
Arthur <arthurg.gmarques@gmail.com>
2025-03-01 16:25:14 -0300

    Atualizado o o estilo do carrinho de compras.

--- frontend/components/Aside.jsx
+++ frontend/components/Aside.jsx
@@ -7,36 +7,78 @@ import { CircleUserRound, ShoppingCartIcon } from 'lucide-react'
 import { useState } from 'react'
 
 const Aside = () => {
-  const [items, setItems] = useState(0)
+  const [items, setItems] = useState([{nome: "X-salada", desc: "Hamburguer de salada", preco: 12.00, img: "images/xbacon.jpeg"}, {nome: "X-bacon", desc: "Hamburguer de bacon", preco: 12.00, img: "images/xbacon.jpeg"}, {nome: "X-egg", desc: "Hamburguer de ovo", preco: 12.00, img: "images/xbacon.jpeg"}])
   
     function renderItems() {
-      setItems(+1)
+      setItems([1])
     }
 
   return (
     <aside className='bg-zinc-950 md:w-[20vw] h-full flex flex-col items-center gap-2'>
-      <div className='bg-zinc-900 w-full lg:h-[51vh] flex flex-col rounded-r-lg p-4 justify-between'>
-        <div className='p-2 flex flex-col gap-1'>
+      <div className='bg-zinc-900 w-full lg:h-[51vh] flex flex-col rounded-r-lg pt-4 justify-between overflow-hidden'>
+        <div className='p-2 flex gap-2 items-center mb-4'>
           <CircleUserRound className='w-10 h-10'></CircleUserRound>
-          <p>
-            Perfil
-          </p>
-        </div>
-        <div className='p-2 flex flex-col gap-1 relative'>
-          <ShoppingCartIcon className='w-10 h-10'></ShoppingCartIcon>
-
-          {
-            items > 0 &&(
-          <div className='absolute w-5 h-5 bg-red-700 left-12 top-8 rounded-full text-center'>
-            <p className='flex items-center justify-center text-xs text-center mt-1'>
-              {items}
+          <div>
+            <h1 className='text-lg'>
+              Perfil
+            </h1>
+            <p className='text-zinc-400 text-sm leading-3'>
+              Descrição
             </p>
           </div>
-            )
-          }
-          <p>
+        </div>
+        <div className='bg-zinc-800 w-full h-full flex flex-col items-center'>
+        <div className='w-full text-center bg-zinc-800 pb-2'>
+          <h2 className='mt-2'>
             Carrinho
-          </p>
+          </h2>
+        </div>
+        <ScrollArea className="h-80 w-full">
+          <div className='flex flex-col items-center justify-center'>
+            {
+              items.length > 0 && (
+              items.map((item, index)=>(
+                <div className='mt-4 w-[90%] bg-zinc-900 h-16 rounded-md flex gap-2 pr-2 lg:pl-2 xl:pl-0'
+                key={index}
+                >
+                  <img src={item.img} 
+                  className='xl:w-[25%] lg:hidden xl:block h-full rounded-md '
+                  />
+
+                  <div className='flex flex-col xl:w-[50%] lg:w-[75%]'>
+                    <h3>
+                      {item.nome}
+                    </h3>
+                    <p className='text-xs text-zinc-400'>
+                      {
+                        item.desc
+                      }
+                    </p>
+                  </div>
+                  <div className='flex flex-col items-center justify-start mt-1'>
+                    <p className='text-sm'>
+                      Preço:
+                    </p>
+                    <p className='text-xs'>
+                      R${item.preco.toFixed(2)}
+                    </p>
+                  </div>
+                </div>
+                )
+              ))
+            }
+        </div>
+        {
+          items.length == 0 && (
+            <div className='flex flex-col items-center gap-2'>
+              <p className='text-zinc-400 text-sm'>
+                Sem itens no carrinho
+              </p>
+              <ShoppingCartIcon className='text-zinc-500 w-10 h-10'></ShoppingCartIcon>
+            </div>
+          )
+        }
+        </ScrollArea>
         </div>
       </div>
 
