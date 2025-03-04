12b827ae4fac1b512ecf54f9f06e4c5e80502680
Arthur <arthurg.gmarques@gmail.com>
2025-03-04 19:59:43 -0300

    Feito os esqueletos de carregamento e frases.

--- frontend/components/Aside.jsx
+++ frontend/components/Aside.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Input } from '@/app/components/ui/input'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 import { ScrollArea } from './ui/scroll-area'
 import { CircleUserRound, ShoppingCartIcon } from 'lucide-react'
