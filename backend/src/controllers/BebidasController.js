00e02493b4d732d6a01a37ec4bde233536586449
Arthur <arthurg.gmarques@gmail.com>
2025-05-12 13:31:17 -0300

    Apagado os arquivos para substituição.
    Apagado os itens que irão ser substituídos para outra versão melhorada.

--- backend/src/controllers/BebidasController.js
+++
@@ -1,9 +0,0 @@
-import BebidasModel from "../models/BebidasSchema.js";
-import Services from "../../services/ServicesController.js";
-
-class BebidasController extends Services{
-    constructor(){
-        super(BebidasModel)
-    } 
-}
-export default new BebidasController()
