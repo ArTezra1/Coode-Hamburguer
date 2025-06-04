8cb7336c62e0bbfa7c31bab378026936ea1ea52e
Arthur <arthurg.gmarques@gmail.com>
2025-06-04 19:35:20 -0300

    Atualizado o next.

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et similique eaque molestiae perspiciatis facilis? Accusantium, sit eos? Neque tenetur soluta earum temporibus id, recusandae voluptate dignissimos adipisci sunt maiores. Repellendus.
+    </div>
+  );
+}
