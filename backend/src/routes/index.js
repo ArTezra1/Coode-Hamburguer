6dbfdc1164e1a3e740edc6137216461c2dba97f6
Arthur <arthurg.gmarques@gmail.com>
2025-06-16 17:33:54 -0300

    Deletado os arquivos que não são mais precisos.

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,16 +1,12 @@
 import user from "./UserRoutes.js"
 import address from "./AddressRoutes.js"
 import product from "./ProductRoutes.js"
-import burger from "./BurgerRoutes.js"
-import drink from "./DrinkRoutes.js"
-import portion from "./PortionRoutes.js"
-import combo from "./ComboRoutes.js"
 import order from "./OrderRoutes.js"
 import sales from "./SalesRoutes.js"
 import salesSummary from "./SalesSummaryRoutes.js"
 
 const routes = (app) => {
-    app.use("/v1", user, address, burger, drink, portion, combo, order, sales, salesSummary, product)
+    app.use("/v1", user, address, order, sales, salesSummary, product)
 };
 
 export default routes
