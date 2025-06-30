5e1663598adc55afa30fd54489b13cb321caf811
Arthur <arthurg.gmarques@gmail.com>
2025-06-30 11:13:30 -0300

    Começado a estilização da Nav.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import HomeIcon from '@mui/icons-material/Home';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import PersonIcon from '@mui/icons-material/Person';
+
+
 const NavBar = () => {
     return (
-        <nav className="block lg:hidden bg-[#1D2C40] w-[96vw] h-12 rounded-xl fixed bottom-1 left-1/2 transform -translate-x-1/2 px-4 py-2">
-            navbar
+        <nav className="flex justify-between items-center lg:hidden bg-[#1D2C40] w-[96vw] h-12 rounded-xl fixed bottom-1 left-1/2 transform -translate-x-1/2 px-4 sm:px-20 py-2">
+            <div>
+                <HomeIcon />
+            </div>
+            <div>
+                <ShoppingCartIcon />
+            </div>
+            <div>
+                <ReceiptIcon />
+            </div>
+            <div>
+                <PersonIcon />
+            </div>
         </nav>
     )
 }
