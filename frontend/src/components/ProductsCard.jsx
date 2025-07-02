32a19c92c1a587653c75a6f46c250e45523401b4
Arthur <arthurg.gmarques@gmail.com>
2025-07-01 21:33:04 -0300

    Terminado os cards dos produtos.
    - Criar a função de adicionar produtos ao carrinho.
    - Criar a função de remover produtos do carrinho.
    - Criar o carrinho para o localStorage.

--- frontend/src/components/ProductsCard.jsx
+++ frontend/src/components/ProductsCard.jsx
@@ -3,17 +3,53 @@ import React from 'react'
 
 const ProductsCard = ({ products = [] }) => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 justify-center gap-4 w-full scroll-pb-32">
+    <div className="grid grid-cols-1 lg:grid-cols-2 justify-center gap-4 w-full lg:pb-8">
       {products.map((product) => (
-        <div key={product._id} className="border rounded-md w-full flex">
-            <Image 
+        <div key={product._id} className="border border-zinc-700 rounded-md w-full flex h-40">
+          <Image
             src={product.imageSrc}
-            width={100}
-            height={100}
+            width={160}
+            height={160}
             alt={product.name}
-            className='rouded-md'
-            />
-          <h3>{product.name.charAt(0).toUpperCase() + product.name.slice(1)}</h3>
+            className='rounded-md border border-zinc-700'
+          />
+          <div className='px-4 py-2 relative w-full'>
+            <h3 className='font-semibold text-md lg:text-lg'>
+              {product.name.charAt(0).toUpperCase() + product.name.slice(1)}
+            </h3>
+            <p className='text-sm lg:text-base'>
+              {product.description.charAt(0).toUpperCase() + product.description.slice(1)}
+            </p>
+            {console.log(product.ingredients)}
+            <p className='text-xs lg:text-sm text-zinc-400 mt-1 lg:mt-0'>
+              {
+                product.category === "burger" && `Ingredientes: ${product.ingredients.map((e) => {
+                  return " " + e.charAt(0).toUpperCase() + e.slice(1)
+                })}`
+                ||
+                product.category === "drink" && `Marca: ${product.brand}`
+                ||
+                product.category === "combo" && `Itens: ${product.itensCombo}`
+              }
+            </p>
+            <div className='absolute bottom-2 flex justify-between w-[90%]'>
+              <h4 className='text-lg font-semibold text-emerald-400'>
+                R$: {product.price.toFixed(2).replace(".", ",")}
+              </h4>
+              <div className='flex gap-4 items-center justify-center'>
+                <button className='px-4 border border-emerald-700 bg-emerald-400 rounded-md text-black hover:scale-105 transition-all duration-300 cursor-pointer font-bold'
+                  onClick={() => addProduct(product._id, product.price)}
+                >
+                  +
+                </button>
+                <button className='px-4 border border-orange-700 bg-orange-600 rounded-md text-black hover:scale-105 transition-all duration-300 cursor-pointer font-bold'
+                onClick={() => removeProduct(product._id, product.price)}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       ))}
     </div>
